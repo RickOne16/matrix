209142545305ca940375b6ea3a1b1c37745fd1f6
Mudit Sharma <sharma.mudit16@gmail.com>
2016-12-03 16:45:20 +0530

    iterative sparse

---
+++ iterative/arnoldi_gram_schmidt.m
@@ -0,0 +1,43 @@
+function [H,V] = arnoldi_gram_schmidt(A,v,m)
+
+## Arnoldi Gram - Schmidt , algorithm 6.2
+## made by : Mudit Sharma
+  
+  n = length(A);
+  if( nargin < 3 ) m = n; 
+  endif
+
+ [size_m, size_n]  =  size (A);
+ if ( size_m != size_n ) error ( " enter a square matrix A ");
+ endif
+
+ if ( norm(v) != 1 ) error (" enter unity norm vector v" );
+ endif
+  
+ V = zeros( n, m+1 );
+ H = zeros( m+1, m );
+
+  V(:,1) = v;
+
+   for j = 1:m
+      
+      w = A * V(:,j);
+     
+      for i = 1:j
+        
+        H(i,j) = V(:,i)' * w;
+        w = w - V(:,i) * H(i,j);
+
+      endfor
+    
+      H(j+1,j) = norm(w);
+      if(!H(j+1,j)) error (" H is not an upper hessenberg matrix!");
+      endif
+
+      V(:,j+1) = w / H(j+1,j);
+
+   endfor
+   V
+endfunction
+
+
---
+++ iterative/gmres.m
@@ -0,0 +1,56 @@
+function [] = gmres ( A, b , x0 , m )
+
+## GMRES algorithm 6.9
+## made by Mudit Sharma
+
+  n = length(A);
+  if( nargin < 3 )
+     m = n; 
+     x0 = [0 ; 0];
+  endif
+  
+  if ( nargin < 4 )
+     m = n;
+  endif
+
+ [size_m, size_n]  =  size (A);
+ if ( size_m != size_n ) error ( " enter a square matrix A ");
+ endif
+  
+ V = zeros( n, m+1 );
+ H = zeros( m+1, m );
+
+ r0 = b - (A * x0);
+ be = norm( r0 );
+ v = r0 / be;
+
+ V(:,1) = v;
+
+   for j = 1:m
+      
+      w = A * V(:,j);
+     
+      for i = 1:j
+        
+        H(i,j) = V(:,i)' * w;
+        w = w - V(:,i) * H(i,j);
+
+      endfor
+    
+      H(j+1,j) = norm(w);
+      
+      if(!H(j+1,j)) 
+        m = j;
+        break; 
+      endif
+
+      V(:,j+1) = w / H(j+1,j);
+   
+  endfor
+  
+  ## Calculate argmin(y) for the function ||b - Ax0 - AVy|| (equation 6.28)
+  ## which algorithm to be used 5.3 or 5.4 ?
+  
+endfunction 
+
+ 
